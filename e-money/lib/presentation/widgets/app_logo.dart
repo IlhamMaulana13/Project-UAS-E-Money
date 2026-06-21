5cd386850e3a1c5d0394f8fadd2fe42fd94a2416
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-06-21 09:29:25 +0700

    Revisi logo domper

--- e-money/lib/presentation/widgets/app_logo.dart
+++ e-money/lib/presentation/widgets/app_logo.dart
@@ -14,8 +14,8 @@ class AppLogo extends StatelessWidget {
 
     Widget icon = Image.asset(
       'assets/images/logo-dompet.png',
-      width: size,
-      height: size,
+      width: 120,
+      height: 120,
       fit: BoxFit.contain,
     );
 
