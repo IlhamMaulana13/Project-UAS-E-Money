4970a8bf96445483cad9ad5dae9777f6a5e92fd9
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-06-25 10:54:25 +0700

    menambah deeplink service

---
+++ e-money/lib/core/services/deeplink_service.dart
@@ -0,0 +1,78 @@
+import 'dart:async';
+import 'package:app_links/app_links.dart';
+
+class DeeplinkPaymentData {
+  final String merchantId;
+  final String merchantName;
+  final double amount;
+  final String description;
+  final String reference;
+  final String callbackUrl;
+
+  DeeplinkPaymentData({
+    required this.merchantId,
+    required this.merchantName,
+    required this.amount,
+    required this.description,
+    required this.reference,
+    required this.callbackUrl,
+  });
+}
+
+class DeeplinkService {
+  static final DeeplinkService _instance = DeeplinkService._internal();
+  factory DeeplinkService() => _instance;
+  DeeplinkService._internal();
+
+  final AppLinks _appLinks = AppLinks();
+  StreamSubscription<Uri>? _linkSubscription;
+
+  final _paymentDataController =
+      StreamController<DeeplinkPaymentData>.broadcast();
+  Stream<DeeplinkPaymentData> get onPaymentReceived =>
+      _paymentDataController.stream;
+
+  Future<void> init() async {
+    try {
+      final initialUri = await _appLinks.getInitialLink();
+      if (initialUri != null) _handleUri(initialUri);
+    } catch (e) {}
+
+    _linkSubscription = _appLinks.uriLinkStream.listen((Uri? uri) {
+      if (uri != null) _handleUri(uri);
+    });
+  }
+
+  void _handleUri(Uri uri) {
+    if (uri.scheme == 'dompetkampus' && uri.host == 'pay') {
+      final params = uri.queryParameters;
+
+      final merchantId = params['merchant_id'] ?? '';
+      final merchantName = params['merchant_name'] ?? 'Merchant Tidak Dikenal';
+      final amountStr = params['amount'] ?? '0';
+      final description = params['description'] ?? 'Pembayaran';
+      final reference = params['reference'] ?? '';
+      final callbackUrl = params['callback'] ?? '';
+
+      final amount = double.tryParse(amountStr) ?? 0.0;
+
+      if (merchantId.isNotEmpty && amount > 0) {
+        final paymentData = DeeplinkPaymentData(
+          merchantId: merchantId,
+          merchantName: merchantName,
+          amount: amount,
+          description: description,
+          reference: reference,
+          callbackUrl: callbackUrl,
+        );
+
+        _paymentDataController.add(paymentData);
+      }
+    }
+  }
+
+  void dispose() {
+    _linkSubscription?.cancel();
+    _paymentDataController.close();
+  }
+}
